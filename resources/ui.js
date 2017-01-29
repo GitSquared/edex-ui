19f3ff364294185831e0f8acdc53ba5b30acfb8e
GitSquared <gitsquared@users.noreply.github.com>
2017-01-29 12:56:49 +0100

    Console log now printed in shell

--- resources/ui.js
+++ resources/ui.js
@@ -1,2 +1,10 @@
+(function () {
+    var $ = require('jquery');
+    var originallog = console.log;
+    console.log = function(txt) {
+        $( "#main_shell > pre" ).append(txt+"<br>");
+        originallog(txt);
+    }
+})();
 require('./resources/sections/main_shell.js')
 require('./resources/sections/keyboard.js')
