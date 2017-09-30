60faf58211460b2c6d10ba7057e704b723eaad4a
GitSquared <GitSquared@users.noreply.github.com>
2017-09-30 16:57:42 +0200

    Fully working TTY (css ain't here yet)

--- _boot.js
+++ _boot.js
@@ -1,11 +1,47 @@
 const {app, BrowserWindow} = require("electron");
+
+console.log(`
+    ===========================================================
+              Starting eDEX-UI v${app.getVersion()} with Node ${process.versions.node}
+                           on Electron ${process.versions.electron}
+    ===========================================================`);
+
 const electron = require("electron");
+const pty = require("node-pty");
+const ws = require("ws").Server;
 const path = require("path");
 const url = require("url");
 
-let win;
+let win, wss, tty;
 
 app.on('ready', () => {
+    tty = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+        name: 'xterm-color',
+        cols: 80,
+        rows: 24,
+        cwd: process.env.PWD,
+        env: process.env
+    });
+
+    wss = new ws({port: 3000});
+    wss.on('connection', (ws) => {
+        console.log("connected");
+        ws.on('message', (msg) => {
+            tty.write(msg);
+        });
+        tty.on('data', (data) => {
+            try {
+                ws.send(data);
+            } catch (e) {
+                // Websocket closed
+            }
+        });
+    });
+    wss.on('close', () => {
+        console.log("closed");
+        tty.kill();
+    });
+
     let {x, y, width, height} = electron.screen.getPrimaryDisplay().bounds;
     width++; height++;
     win = new BrowserWindow({
--- _renderer.js
+++ _renderer.js
@@ -1,41 +1,9 @@
-const {desktopCapturer} = require('electron');
+const Terminal = require("xterm");
+Terminal.loadAddon('attach');
 
-desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
-  if (error) throw error;
-  window.sources = sources;
-  for (let i = 0; i < sources.length; ++i) {
-    if (sources[i].name === 'eDEX-UI') {
-        navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: sources[i].id,
-              minWidth: 1280,
-              maxWidth: 1280,
-              minHeight: 720,
-              maxHeight: 720
-            }
-          }
-        }).then((mediaStream) => {
-          let video = document.querySelector('video');
-          video.srcObject = mediaStream;
-          video.onloadedmetadata = () => {
-            video.play();
-          };
-        })
-        .catch((err) => { console.log(err.name + ": " + err.message); });
-      return;
-    }
-  }
-});
-
-function handleStream (stream) {
-console.log(stream);
-console.log(URL.createObjectURL(stream));
-  document.querySelector('video').src = URL.createObjectURL(stream);
-}
-
-function handleError (e) {
-  console.log(e);
-}
+let term = new Terminal();
+term.open(document.getElementById("terminal"));
+let socket = new WebSocket("ws://0.0.0.0:3000");
+socket.onopen = () => {
+    term.attach(socket);
+};
