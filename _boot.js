eeb3eb7637b6aa970aa017367d48cc21288cd73a
PixelyIon <dhruvmarkcollins@gmail.com>
2017-10-02 02:33:42 +0530

    Some minor changes
    Changelog:
    cmd.exe changed to powershell.exe
    bg.png replaced with native CSS solution
    variables.css added with only two variables atm
    additional formatting added to fonts.css

--- _boot.js
+++ _boot.js
@@ -18,7 +18,7 @@ let settingsFile = path.join(__dirname, "settings.json");
 
 if (!fs.existsSync(settingsFile)) {
     fs.writeFileSync(settingsFile, JSON.stringify({
-        shell: (process.platform === "win32") ? "cmd.exe" : "bash",
+        shell: (process.platform === "win32") ? "powershell.exe" : "bash",
         keyboard: "EN-us"
     }));
 }
