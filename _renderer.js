433ce330b3b75b85993c82a33ff29fe7e0e3a8e7
GitSquared <GitSquared@users.noreply.github.com>
2017-10-01 15:10:05 +0200

    Prevent unwanted keyboard quit

--- _renderer.js
+++ _renderer.js
@@ -5,3 +5,16 @@ window.term = new Terminal({
 setTimeout(() => {
     window.term.fit();
 }, 500);
+
+// Prevent showing menu, exiting fullscreen or app with keyboard shortcuts
+window.onkeydown = (e) => {
+    if (e.key === "Alt" || e.key === "F11") {
+        e.preventDefault();
+    }
+    if (e.code === "KeyD" && e.ctrlKey) {
+        e.preventDefault();
+    }
+    if (e.code === "KeyA" && e.ctrlKey) {
+        e.preventDefault();
+    }
+};
