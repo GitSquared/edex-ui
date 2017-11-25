ecc8e62bd48ba1f5149233504615212fbd2919bb
GitSquared <GitSquared@users.noreply.github.com>
2017-11-25 11:12:08 +0100

    Bind real keyboard to on-screen animations

--- src/classes/keyboard.class.js
+++ src/classes/keyboard.class.js
@@ -84,6 +84,63 @@ class Keyboard {
                 }
             });
         });
+
+        // Bind actual keyboard actions to on-screen animations (for use without a touchscreen)
+        let findKey = (e) => {
+            // Find basic keys (typically letters, upper and lower-case)
+            let key = document.querySelector('div.keyboard_key[data-cmd="'+e.key+'"]');
+            if (key === null) key = document.querySelector('div.keyboard_key[data-shift_cmd="'+e.key+'"]');
+
+            // Find special keys (shift, control, enter, etc.)
+            if (key === null && e.code === "ShiftLeft") key = document.querySelector('div.keyboard_key[data-cmd="ESCAPED|-- SHIFT: LEFT"]');
+            if (key === null && e.code === "ShiftRight") key = document.querySelector('div.keyboard_key[data-cmd="ESCAPED|-- SHIFT: RIGHT"]');
+            if (key === null && e.code === "ControlLeft") key = document.querySelector('div.keyboard_key[data-cmd="ESCAPED|-- CTRL: LEFT"]');
+            if (key === null && e.code === "ControlRight") key = document.querySelector('div.keyboard_key[data-cmd="ESCAPED|-- CTRL: RIGHT"]');
+            if (key === null && e.code === "AltLeft") key = document.querySelector('div.keyboard_key[data-cmd="ESCAPED|-- FN: ON"]');
+            if (key === null && e.code === "AltRight") key = document.querySelector('div.keyboard_key[data-cmd="ESCAPED|-- ALT: RIGHT"]');
+            if (key === null && e.code === "CapsLock") key = document.querySelector('div.keyboard_key[data-cmd="ESCAPED|-- CAPSLCK: ON"]');
+            if (key === null && e.code === "Escape") key = document.querySelector('div.keyboard_key[data-cmd=""]');
+            if (key === null && e.code === "Backspace") key = document.querySelector('div.keyboard_key[data-cmd=""]');
+            if (key === null && e.code === "Enter") key = document.querySelectorAll('div.keyboard_key.keyboard_enter');
+
+            // Find "rare" keys (ctrl and alt symbols)
+            if (key === null) key = document.querySelector('div.keyboard_key[data-ctrl_cmd="'+e.key+'"]');
+            if (key === null) key = document.querySelector('div.keyboard_key[data-alt_cmd="'+e.key+'"]');
+
+            return key;
+        }
+
+        document.onkeydown = (e) => {
+            let key = findKey(e);
+            if (key === null) return;
+            if (key.length) {
+                key.forEach((enterElement) => {
+                    enterElement.setAttribute("class", "keyboard_key active keyboard_enter");
+                });
+            } else {
+                key.setAttribute("class", "keyboard_key active");
+            }
+        }
+
+        document.onkeyup = (e) => {
+            let key = findKey(e);
+            if (key === null) return;
+            if (key.length) {
+                key.forEach((enterElement) => {
+                    enterElement.setAttribute("class", "keyboard_key blink keyboard_enter");
+                });
+                setTimeout(() => {
+                    key.forEach((enterElement) => {
+                        enterElement.setAttribute("class", "keyboard_key keyboard_enter");
+                    });
+                }, 100);
+            } else {
+                key.setAttribute("class", "keyboard_key blink");
+                setTimeout(() => {
+                    key.setAttribute("class", "keyboard_key");
+                }, 100);
+            }
+        }
     }
 }
 
