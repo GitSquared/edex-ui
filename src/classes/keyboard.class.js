74611942bef5cd9ce23a672f644c3ce0b18a7c50
Gabriel SAILLARD (GitSquared) <gabriel@saillard.dev>
2019-03-17 19:16:40 +0100

    :art: Replace arrow emojis by proper icon support
    ...on keyboard layouts. Closes #462.

--- src/classes/keyboard.class.js
+++ src/classes/keyboard.class.js
@@ -25,6 +25,7 @@ class Keyboard {
         Object.keys(layout).forEach(row => {
             this.container.innerHTML += `<div class="keyboard_row" id="`+row+`"></div>`;
             layout[row].forEach(keyObj => {
+
                 let key = document.createElement("div");
                 key.setAttribute("class", "keyboard_key");
 
@@ -40,6 +41,31 @@ class Keyboard {
                         <h3>${keyObj.alt_name || ""}</h3>
                         <h2>${keyObj.shift_name || ""}</h2>
                         <h1>${keyObj.name || ""}</h1>`;
+                }
+
+                // Icon support, overrides previously defined innerHTML
+                // Arrow and other icons
+                let icon = null;
+                if (keyObj.name.startsWith("ESCAPED|-- ICON: ")) {
+                    keyObj.name = keyObj.name.substr(17);
+                    switch(keyObj.name) {
+                        case "ARROW_UP":
+                            icon = `<svg viewBox="0 0 24.00 24.00"><path fill-opacity="1" d="m12.00004 7.99999 4.99996 5h-2.99996v4.00001h-4v-4.00001h-3z"/><path stroke-linejoin="round" fill-opacity="0.65" d="m4 3h16c1.1046 0 1-0.10457 1 1v16c0 1.1046 0.1046 1-1 1h-16c-1.10457 0-1 0.1046-1-1v-16c0-1.10457-0.10457-1 1-1zm0 1v16h16v-16z"/></svg>`;
+                            break;
+                        case "ARROW_LEFT":
+                            icon = `<svg viewBox="0 0 24.00 24.00"><path fill-opacity="1" d="m7.500015 12.499975 5-4.99996v2.99996h4.00001v4h-4.00001v3z"/><path stroke-linejoin="round" fill-opacity="0.65" d="m4 3h16c1.1046 0 1-0.10457 1 1v16c0 1.1046 0.1046 1-1 1h-16c-1.10457 0-1 0.1046-1-1v-16c0-1.10457-0.10457-1 1-1zm0 1v16h16v-16z"/></svg>`;
+                            break;
+                        case "ARROW_BOTTOM":
+                            icon = `<svg viewBox="0 0 24.00 24.00"><path fill-opacity="1" d="m12 17-4.99996-5h2.99996v-4.00001h4v4.00001h3z"/><path stroke-linejoin="round" fill-opacity="0.65" d="m4 3h16c1.1046 0 1-0.10457 1 1v16c0 1.1046 0.1046 1-1 1h-16c-1.10457 0-1 0.1046-1-1v-16c0-1.10457-0.10457-1 1-1zm0 1v16h16v-16z"/></svg>`;
+                            break;
+                        case "ARROW_RIGHT":
+                            icon = `<svg viewBox="0 0 24.00 24.00"><path fill-opacity="1" d="m16.500025 12.500015-5 4.99996v-2.99996h-4.00001v-4h4.00001v-3z"/><path stroke-linejoin="round" fill-opacity="0.65" d="m4 3h16c1.1046 0 1-0.10457 1 1v16c0 1.1046 0.1046 1-1 1h-16c-1.10457 0-1 0.1046-1-1v-16c0-1.10457-0.10457-1 1-1zm0 1v16h16v-16z"/></svg>`;
+                            break;
+                        default:
+                            icon = `<svg viewBox="0 0 24.00 24.00"><path fill="#ff0000" fill-opacity="1" d="M 8.27125,2.9978L 2.9975,8.27125L 2.9975,15.7275L 8.27125,21.0012L 15.7275,21.0012C 17.485,19.2437 21.0013,15.7275 21.0013,15.7275L 21.0013,8.27125L 15.7275,2.9978M 9.10125,5L 14.9025,5L 18.9988,9.10125L 18.9988,14.9025L 14.9025,18.9988L 9.10125,18.9988L 5,14.9025L 5,9.10125M 9.11625,7.705L 7.705,9.11625L 10.5912,12.0025L 7.705,14.8825L 9.11625,16.2937L 12.0025,13.4088L 14.8825,16.2937L 16.2938,14.8825L 13.4087,12.0025L 16.2938,9.11625L 14.8825,7.705L 12.0025,10.5913"/></svg>`;
+                    }
+
+                    key.innerHTML = icon;
                 }
 
                 Object.keys(keyObj).forEach(property => {
