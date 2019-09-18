98d5652fab5551fa3de6d9ed4d46d13ca10cf29f
GitSquared <gabriel@saillard.dev>
2019-09-18 17:53:27 +0200

    :mute: Remove junk log

--- src/assets/vendor/encom-globe.js
+++ src/assets/vendor/encom-globe.js
@@ -23055,8 +23055,6 @@ THREE.ShaderLib = {
 
 THREE.WebGLRenderer = function ( parameters ) {
 
-	console.log( 'THREE.WebGLRenderer', THREE.REVISION );
-
 	parameters = parameters || {};
 
 	var _canvas = parameters.canvas !== undefined ? parameters.canvas : document.createElement( 'canvas' ),
