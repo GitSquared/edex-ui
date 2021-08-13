cee0fe06d32114367afc52371190288d8523a886
Mike Stay <stay@pyrofex.net>
2021-08-13 11:32:30 -0600

    Add audio icon, fix parsing of bytesize-icon dimensions

--- file-icons-generator.js
+++ file-icons-generator.js
@@ -108,10 +108,10 @@ fs.readdirSync(path.join(__dirname, "file-icons", "bytesize-icons", "dist", "ico
 
     let text = fs.readFileSync(path.join(__dirname, "file-icons", "bytesize-icons", "dist", "icons", icon), {encoding: "utf8"});
 
-    let width = text.substr(text.indexOf('width="')+7);
-    width = Number(width.slice(0, width.indexOf("px")));
-    let height = text.substr(text.indexOf('height="')+8);
-    height = Number(height.slice(0, height.indexOf("px")));
+    let dimensions = text.match(/viewBox="0 0 (\d+) (\d+)"/);
+    let height = dimensions[1];
+    let width = dimensions[1];
+
     let svg = text.substr(text.indexOf(">")+1);
     svg = svg.replace("</svg>", "");
 
@@ -169,6 +169,7 @@ fileIconsObject.usb = {
     height: 24,
     svg: '<path d="M 14.9994,6.99807L 14.9994,10.9981L 15.9994,10.9981L 15.9994,12.9981L 12.9994,12.9981L 12.9994,4.99807L 14.9994,4.99807L 11.9994,0.998068L 8.9994,4.99807L 10.9994,4.99807L 10.9994,12.9981L 7.9994,12.9981L 7.9994,10.9281C 8.7034,10.5611 9.1994,9.84707 9.1994,8.99807C 9.1994,7.78307 8.2144,6.79807 6.9994,6.79807C 5.7844,6.79807 4.7994,7.78307 4.7994,8.99807C 4.7994,9.84707 5.2954,10.5611 5.9994,10.9281L 5.9994,12.9981C 5.9994,14.1031 6.8934,14.9981 7.9994,14.9981L 10.9994,14.9981L 10.9994,18.0491C 10.2894,18.4141 9.7994,19.1451 9.7994,19.9981C 9.7994,21.2131 10.7844,22.1981 11.9994,22.1981C 13.2144,22.1981 14.1994,21.2131 14.1994,19.9981C 14.1994,19.1451 13.7084,18.4141 12.9994,18.0491L 12.9994,14.9981L 15.9994,14.9981C 17.1044,14.9981 17.9994,14.1031 17.9994,12.9981L 17.9994,10.9981L 18.9994,10.9981L 18.9994,6.99807L 14.9994,6.99807 Z"/>'
 };
+fileIconsObject.audio = fileIconsObject.volume;
 // Write the file
 fs.writeFileSync(path.join(__dirname, "src", "assets", "icons", "file-icons.json"), JSON.stringify(fileIconsObject, "", 4));
 console.log("Wrote file-icons.json");
